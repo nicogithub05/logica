cfc85ede1a33b4da2c3ffb18284e07b935734c05
nivamol <nicolasgithub@gmail.com>
2026-06-12 15:12:34 -0500

    ejercicio 1 resuelto

---
+++ exercise1.js
@@ -0,0 +1,22 @@
+
+//1. Mostrar un prompt para pedir el nombre del usuario.
+//2. Guardar el nombre en una variable.
+//3. Imprimir un mensaje de saludo con el nombre.
+
+
+//opcion 1 
+let nombre = prompt('Ingresa tu nombre: ');
+console.log(`Hola, me llamo ${nombre}`);
+
+
+
+
+// opcion 2
+let imprimirNombre = (nombre) => console.log(`Hola, me llamo ${nombre}`)
+
+imprimirNombre('Nicolás');
+
+
+
+
+
