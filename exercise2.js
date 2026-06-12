cb57ada9573469da1b861c83b8cd739e981c96fe
nivamol <nicolasgithub@gmail.com>
2026-06-12 15:39:44 -0500

    ejercicio 2 resuelto

---
+++ exercise2.js
@@ -0,0 +1,15 @@
+/*
+1. Solicitar dos números mediante prompts.
+2. Convertir los inputs a números flotantes.
+3. Sumar ambos números.
+4. Mostrar el resultado.
+*/
+
+let num1 = prompt('Ingrese el primer número:')
+let num2 = prompt('Ingres el segundo número: '); 
+
+let suma = parseFloat(num1) + parseFloat(num2);
+console.log(`el resultado es: ${suma}`);
+
+
+
